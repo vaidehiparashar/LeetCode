a784950903561f914f47f34963adc471e33e7f3c
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-07-25 00:17:19 +0530

    LC-number-of-unique-xor-triplets-ii [Runtime: N/A; Memory: N/A]

--- math/number-of-unique-xor-triplets-ii.java
+++ math/number-of-unique-xor-triplets-ii.java
@@ -1,17 +1,38 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
 
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
+        boolean[] present = new boolean[2048];
+        for (int x : nums) {
+            present[x] = true;
+        }
+        int[] values = new int[2048];
+        int m = 0;
+        for (int i = 0; i < 2048; i++) {
+            if (present[i]) {
+                values[m++] = i;
+            }
+        }
+        boolean[] pairXor = new boolean[2048];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                pairXor[values[i] ^ values[j]] = true;
+            }
+        }
+        boolean[] ans = new boolean[2048];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                for (int k = j; k < n; k++) {
-                    set.add(nums[i] ^ nums[j] ^ nums[k]);
-                }
+        for (int x = 0; x < 2048; x++) {
+            if (!pairXor[x]) continue;
+
+            for (int i = 0; i < m; i++) {
+                ans[x ^ values[i]] = true;
             }
         }
 
-        return set.size();
+        int count = 0;
+        for (boolean b : ans) {
+            if (b) count++;
+        }
+
+        return count;
     }
 }
