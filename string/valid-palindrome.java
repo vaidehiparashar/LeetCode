6a936d71dc2a1d43a82d3fccbf632937d4c2e025
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-05 15:50:01 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- string/valid-palindrome.java
+++ string/valid-palindrome.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            char left = Character.toLowerCase(s.charAt(i));
+            char right = Character.toLowerCase(s.charAt(j));
+            if (left != right) {
+                return false;
+            }
+            i++;
+            j--;
         }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++; 
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
-        }
+
         return true;
     }
 }
