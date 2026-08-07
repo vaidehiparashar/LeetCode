5421daecda8d19d4c46e3f6bfcb40bab9dc2d88c
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-07 21:50:51 +0530

    LC-smallest-divisible-digit-product-ii [Runtime: N/A; Memory: N/A]

--- number-theory/smallest-divisible-digit-product-ii.java
+++ number-theory/smallest-divisible-digit-product-ii.java
@@ -1,60 +1,127 @@
 class Solution {
-
-    String num; 
-    int allowedPrimes[] = {2, 3, 5, 7}; 
-    private boolean isReqMet(int[] freq) {
-        for(int p: allowedPrimes) {
-            if(freq[p] > 0) return false; 
+private:
+    bool isInvalid(long long n) {
+        for (int i = 2; i < 10; i++) {
+            while (n % i == 0) {
+                n /= i;
+            }
         }
-        return true; 
+        return n != 1;
     }
-    private StringBuilder getNum(int[] freq, int pos, int len, StringBuilder cur, boolean flag) {
 
-        if (pos == len) {
-            if (isReqMet(freq))
-                return cur;
-            return null;
-        }
-        for(int i = 1; i <= 9; i++) {
-            char c = (char)('0' + i); 
-            if (!flag && pos < num.length() && c < num.charAt(pos))
-                continue; 
-            int curFreq[] = freq.clone(); 
-            for(int _p: allowedPrimes) {
-                int t = i; 
-                while(t % _p == 0) {
-                    curFreq[_p]--; 
-                    t/=_p; 
-                } 
-                curFreq[_p] = Math.max(0, curFreq[_p]); 
-            } 
-            cur.append(c); 
-            StringBuilder res = getNum(curFreq, pos + 1, len, cur, flag || (pos < num.length() && num.charAt(pos) < c)); 
-            if(res != null) return res; 
-            // backtrack 
-            cur.deleteCharAt(cur.length() - 1); 
+    int minDigitsNeeded(long long t) {
+        if (t <= 1) {
+            return 0;
         }
 
-        
-        return null; 
-    }
-    public String smallestNumber(String num, long t) {
-        this.num = num; 
-        int freq[] = new int[10]; 
-        for(int p: allowedPrimes) {
-            while(t % p == 0) {
-                freq[p]++; 
-                t/= p; 
+        int count = 0;
+
+        for (int d = 9; d >= 2; d--) {
+            while (t % d == 0) {
+                count++;
+                t /= d;
             }
         }
 
-        if(t > 1) return "-1"; 
-        StringBuilder ans = new StringBuilder(); 
-        for(int i = num.length(); i < num.length() + 10; i++) {
-            StringBuilder res = getNum(freq, 0, i, ans, i > num.length() ? true :false); // [freq, pos] 
-            if(res != null) return res.toString(); 
-        }
-        
-        return "-1"; 
+        return count;
     }
-}
+
+    string getMore(long long t) {
+        string sol = "";
+
+        while (t != 1) {
+            int curr = 9;
+
+            while (curr > 1 && (t % curr != 0)) {
+                curr--;
+            }
+
+            sol.push_back(char(curr + '0'));
+            t /= curr;
+        }
+
+        reverse(sol.begin(), sol.end());
+        return sol;
+    }
+
+    long long reduceT(long long t, int d) {
+        for (int f = 9; f >= 2; f--) {
+            while (d % f == 0 && t % f == 0) {
+                t /= f;
+                d /= f;
+            }
+        }
+
+        return t;
+    }
+
+public:
+    string smallestNumber(string num, long long t) {
+        int n = num.size();
+
+        if (isInvalid(t)) {
+            return "-1";
+        }
+
+        int zeroIdx = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (num[i] == '0') {
+                zeroIdx = i;
+                break;
+            }
+        }
+
+        if (zeroIdx == -1) {
+            long long tempT = t;
+
+            for (char c : num) {
+                tempT = reduceT(tempT, c - '0');
+            }
+
+            if (tempT == 1) {
+                return num;
+            }
+        }
+
+        int maxPrefix = (zeroIdx == -1) ? n : zeroIdx;
+
+        vector<long long> prefixT(n + 1, t);
+
+        for (int i = 0; i < maxPrefix; i++) {
+            prefixT[i + 1] = reduceT(prefixT[i], num[i] - '0');
+        }
+
+        for (int i = maxPrefix - 1; i >= 0; i--) {
+            int remainingLen = n - 1 - i;
+            int startDigit = (num[i] - '0') + 1;
+
+            for (int d = startDigit; d <= 9; d++) {
+                long long remT = reduceT(prefixT[i], d);
+
+                if (minDigitsNeeded(remT) <= remainingLen) {
+                    string sol = num.substr(0, i);
+                    sol.push_back(char(d + '0'));
+
+                    string suffix = getMore(remT);
+
+                    while ((int)suffix.size() < remainingLen) {
+                        suffix = "1" + suffix;
+                    }
+
+                    return sol + suffix;
+                }
+            }
+        }
+
+        int reqLen = max(n + 1, minDigitsNeeded(t));
+
+        string sol = getMore(t);
+
+        while ((int)sol.size() < reqLen) {
+            sol = "1" + sol;
+        }
+
+        return sol;
+    }
+};
