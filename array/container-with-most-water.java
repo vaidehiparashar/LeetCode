0aac8bdb1deb953507db58be49a8329f7c513ac5
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-11 16:22:27 +0530

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

--- array/container-with-most-water.java
+++ array/container-with-most-water.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
+        int right = height.length-1;
         int left = 0;
-        int right = height.length - 1;
-        int max = 0;
-        while(left < right){
-            int w = right - left;
-            int h = Math.min(height[left], height[right]);
-            int area = h * w;
-            max = Math.max(max, area);
-            if(height[left] < height[right]) left++;
-            else if(height[left] > height[right]) right--;
-            else {
+        int maxArea  = 0;
+        while(left<right){
+            maxArea = Math.max(maxArea, (right- left)* Math.min(height[left], height[rigth]));
+            if(heigth[left]<heigth[right]){
                 left++;
-                right--;
+            } else {
+                right --;
             }
+
         }
-        return max;
+        return maxArea;       
     }
 }
