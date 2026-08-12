c35b605a6012dc4d164e8a442166ce041f7ee630
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-12 16:35:39 +0530

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

--- hash-table/length-of-longest-subarray-with-at-most-k-frequency.java
+++ hash-table/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -2,17 +2,15 @@ class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
         int n = nums.length;
         int r = 1;
-        for(int left = 0; left<n; left++){
-            HashMap<Integer ,Integer> freq = new HahMap<>();
-            for(int right = left; right < n; rigth++){
-                int c = nums[rigtht];
-                freq.put(c, freq.getOrDefault(c,0)+1));
-                if(freq.put(c)>k) break;
-                r = Math.max(r, rigth - left+1);
-                            }      
-                  }
+        for (int left = 0; left < n; left++) {
+            HashMap<Integer, Integer> freq = new HashMap<>();
+            for (int right = left; right < n; right++) {
+                int c = nums[right];
+                freq.put(c, freq.getOrDefault(c, 0) + 1);
+                if (freq.get(c) > k) break;
+                r = Math.max(r, right - left + 1);
+            }
         }
         return r;
-        
     }
 }
