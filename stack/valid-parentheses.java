f300e4f25fff3b34e6111d164bcaea780274edc9
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-12 17:05:11 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.java
+++ stack/valid-parentheses.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
+
+        HashMap<Character, Character> mapping = new HashMap<>();
+
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> mapping = new HashMap<>();
-        mapping.put ( '(' , ')');
-        mapping.put('{' , '}');
-        mapping.put( '[' , ']');
-        for(int i =0; i<s.length();i++){
+
+        for (int i = 0; i < s.length(); i++) {
+
             char c = s.charAt(i);
-            if(mapping.containsValue(c)){
+
+            if (mapping.containsValue(c)) {
                 stack.push(c);
+            } 
             else if (mapping.containsKey(c)) {
-        if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
-            return false;
+
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
             }
         }
+
+        return stack.isEmpty();
     }
-    return stack.isEmpty();
 }
-    }
