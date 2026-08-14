0dccbc275dcce334e06138969ba7bf59c9ea4aa5
dahiisworking <157959209+vaidehiparashar@users.noreply.github.com>
2026-08-14 23:59:37 +0530

    LC-maximum-length-substring-with-two-occurrences [Runtime: N/A; Memory: N/A]

---
+++ string/maximum-length-substring-with-two-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int res = 0, l = 0;
+        long mask = 0;
+        
+        for (int r = 0; r < s.length(); r++) {
+            int k = (s.charAt(r) & 31) << 1;
+            mask += 1L << k;
+            
+            while (((mask >> k) & 3) == 3)
+                mask -= 1L << ((s.charAt(l++) & 31) << 1);
+                
+            res = Math.max(res, r - l + 1);
+        }
+        
+        return res;
+    }
+}
